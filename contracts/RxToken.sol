6e15fc1635ca1914c190e86b1142e286278f50df
Ares J <jaedagk@live.com>
2018-09-04 06:08:17 -0500

    Added transfer event.

--- contracts/RxToken.sol
+++ contracts/RxToken.sol
@@ -7,6 +7,12 @@ contract RxToken {
     string public symbol = "DRGX";
     string public standard = "Rx Token v1.0";
     uint256 public totalSupply;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
     
     mapping(address => uint256) public balanceOf;
 
@@ -14,5 +20,18 @@ contract RxToken {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
         // allocate the initial supply
+    }
+
+    // Transfer
+    function transfer(address _to, uint256 _value) public returns (bool success){
+    // Exception if account doesn't have enough
+        require(balanceOf[msg.sender] >= _value);
+    // Transfer the balance
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+    // Transfer Event
+        Transfer(msg.sender, _to, _value);
+    // Return a boolean
+        return true;
     }
 }
