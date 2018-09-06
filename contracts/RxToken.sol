cfcbefefc88ad557b76fa6527773229ae0e71c45
Ares <jaedagk@live.com>
2018-09-06 01:22:48 -0500

    3. Delegated Transfer

--- contracts/RxToken.sol
+++ contracts/RxToken.sol
@@ -40,7 +40,7 @@ contract RxToken {
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
     // Transfer Event
-        Transfer(msg.sender, _to, _value);
+        emit Transfer(msg.sender, _to, _value);
     // Return a boolean
         return true;
     }
@@ -50,9 +50,24 @@ contract RxToken {
         // Allowance
         allowance[msg.sender][_spender] = _value;
         // Approve
-        Approval(msg.sender, _spender, _value);
+        emit Approval(msg.sender, _spender, _value);
 
         return true;
     }
     // transferFrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        // Require _from has enough tokens
+        require(_value <= balanceOf[_from]);
+        // Require that the allowance is big enough
+        require(_value <= allowance[_from][msg.sender] );
+        // Change the balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        // Update the allowance
+        allowance[_from][msg.sender] -= _value;
+        // Transfer event
+        emit Transfer(_from, _to, _value);
+        // Return a boolean
+        return true;
+    }
 }
