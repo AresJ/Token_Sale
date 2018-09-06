ba4f5c309fdec9484fe6ec07dd3d53412b95cac7
Ares <jaedagk@live.com>
2018-09-06 03:37:49 -0500

    Created Token Sale items

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var RxToken = artifacts.require("./RxToken.sol");
+var RxTokenSale = artifacts.require("./RxTokenSale.sol");
+
 
 module.exports = function(deployer) {
   deployer.deploy(RxToken, 1000000);
+  deployer.deploy(RxTokenSale);
 };
